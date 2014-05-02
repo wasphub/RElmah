4d72c3e9465801caae6db172faea2a3cd24c1f08
wasp <roby@robychechi.it>
2014-05-02 18:56:03 +0200

    Back to 4.5 + test web api app

---
+++ src/RElmah.Web.Server/Controllers/RElmahController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RElmah.Server;
+using RElmah.Server.Domain;
+
+namespace RElmah.Web.Server.Controllers
+{
+    [Route("/post-error")]
+    public class RElmahController : ApiController
+    {
+        private readonly IErrorsInbox _inbox;
+
+        public RElmahController(IErrorsInbox inbox)
+        {
+            _inbox = inbox;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(string error)
+        {
+            _inbox.Post(new ErrorPayload { Message = error });
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+}
---
+++ src/RElmah.Web.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RElmah.Web.Server.WebApiApplication" Language="C#" %>
