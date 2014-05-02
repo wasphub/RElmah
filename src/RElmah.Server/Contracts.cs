bb6980d80f9165f4ca316e2b9b7d0cdb9fa04424
wasp <roby@robychechi.it>
2014-05-02 15:14:03 +0200

    Rx-Core added

--- src/RElmah.Server/Contracts.cs
+++ src/RElmah.Server/Contracts.cs
@@ -1,9 +1,11 @@
-﻿using RElmah.Server.Domain;
+﻿using System;
+using RElmah.Server.Domain;
 
 namespace RElmah.Server
 {
     public interface IErrorsInbox
     {
         void Post(ErrorDescriptor descriptor);
+        IObservable<ErrorDescriptor> GetErrors();
     }
 }
--- src/RElmah.Server/Services/ErrorsInbox.cs
+++ src/RElmah.Server/Services/ErrorsInbox.cs
@@ -1,13 +1,24 @@
-﻿using RElmah.Server.Domain;
+﻿using System;
+using RElmah.Server.Domain;
 
 namespace RElmah.Server.Services
 {
     class ErrorsInbox : IErrorsInbox
     {
+        public ErrorsInbox()
+        {
+            
+        }
+
         public void Post(ErrorDescriptor descriptor)
         {
             
 
+        }
+
+        public IObservable<ErrorDescriptor> GetErrors()
+        {
+            return null;
         }
     }
 }
