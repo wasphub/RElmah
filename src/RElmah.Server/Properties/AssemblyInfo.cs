c7905fcd2943414d20e3e77733e243aa7436f0c6
wasp <roby@robychechi.it>
2014-05-07 23:53:17 +0200

    Version numbers goes 0.1 for all main projects

--- src/RElmah.Server/Properties/AssemblyInfo.cs
+++ src/RElmah.Server/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("926b3c16-e589-4327-ab36-6a5ab481d5ce")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
