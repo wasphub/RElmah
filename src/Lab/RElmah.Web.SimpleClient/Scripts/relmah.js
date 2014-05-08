000d9c4655cb02a76f07aa0981abdeed38b4c545
wasp <roby@robychechi.it>
2014-05-08 23:20:37 +0200

    First experiments with RxJS

---
+++ src/Lab/RElmah.Web.SimpleClient/Scripts/relmah.js
@@ -0,0 +1,21 @@
+﻿relmah = (function() {
+    "use strict";
+
+    return function (endpoint) {
+        var errors = new Rx.Subject();
+
+        return {
+            start: function() {
+                var conn = $.hubConnection(endpoint),
+                    relmah = conn.createHubProxy('relmah');
+
+                relmah.on('dispatch', function (p) {
+                    errors.onNext(p);
+                });
+
+                return conn.start();
+            },
+            errors: errors
+        };
+    };
+})();
