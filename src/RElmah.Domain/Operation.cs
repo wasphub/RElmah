bfb3cf3c948f0508e0ee70217dc33ae480b3f7c0
wasp <roby@robychechi.it>
2014-05-24 11:49:56 +0200

    More renamings

---
+++ src/RElmah.Domain/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace RElmah.Domain
+{
+    public enum OperationType
+    {
+        Create,
+        Update,
+        Remove
+    }
+
+    public class Operation<T>
+    {
+        public Operation(IEnumerable<T> targets, OperationType type)
+        {
+            Targets = targets;
+            Type = type;
+        }
+
+        public IEnumerable<T> Targets { get; private set; }
+        public OperationType Type { get; private set; }
+    }
+}
