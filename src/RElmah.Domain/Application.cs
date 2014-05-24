2af1e91fce04cb87e5dcd98926a584726ed25e29
wasp <roby@robychechi.it>
2014-05-24 11:48:24 +0200

    Introducing Application domain type

---
+++ src/RElmah.Domain/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace RElmah.Domain
+{
+    public class Application
+    {
+        public string Name { get; set; }
+        public string SourceId { get; set; }
+
+        public Cluster Cluster { get; set; }
+    }
+}
--- src/RElmah.Domain/Cluster.cs
+++ src/RElmah.Domain/Cluster.cs
@@ -1,7 +1,11 @@
-﻿namespace RElmah.Domain
+﻿using System.Collections.Generic;
+
+namespace RElmah.Domain
 {
     public class Cluster
     {
         public string Name { get; set; }
+
+        public IEnumerable<Application> Applications { get; set; }
     }
 }
