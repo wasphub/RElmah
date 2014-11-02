cc77e22229f29620b0b99731cdab9c8d4f4ed0ee
wasp <roby@robychechi.it>
2014-11-02 12:40:35 +0100

    Preparing RElmah Owin middleware

--- relmah/src/RElmah.Host.SignalR/AppBuilderExtensions.cs
+++ relmah/src/RElmah.Host.SignalR/AppBuilderExtensions.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using RElmah.Services;
 
 namespace RElmah.Host.SignalR
 {
     public static class AppBuilderExtensions
     {
+        public static IAppBuilder UseRElmah(this IAppBuilder builder)
+        {
+            var registry = GlobalHost.DependencyResolver;
 
+            registry.Register(typeof(IErrorsInbox), () => new ErrorsInbox());
+
+            return builder.UseRElmahMiddleware<RElmahMiddleware>(GlobalHost.DependencyResolver);
+        }
+
+        static IAppBuilder UseRElmahMiddleware<T>(this IAppBuilder builder, params object[] args)
+        {
+            return builder.Use(typeof(T), args);
+        }
+    }
+
+    public class RElmahMiddleware : OwinMiddleware
+    {
+        public RElmahMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
