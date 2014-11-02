e6c3834098bf615d0fe0c77de653dfeb58414dbd
wasp <roby@robychechi.it>
2014-11-02 17:10:13 +0100

    Null config updater

--- relmah/src/RElmah/Services/ConfigurationHolder.cs
+++ relmah/src/RElmah/Services/ConfigurationHolder.cs
@@ -3,6 +3,7 @@ using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using RElmah.Models;
 using RElmah.Models.Configuration;
+using RElmah.Services.Nulls;
 
 namespace RElmah.Services
 {
@@ -10,23 +11,25 @@ namespace RElmah.Services
     {
         private readonly IConfigurationUpdater _configurationStore;
 
-        private readonly Subject<Delta<Cluster>> _clusterOperations = new Subject<Delta<Cluster>>();
+        private readonly Subject<Delta<Cluster>> _clusterDeltas = new Subject<Delta<Cluster>>();
 
         public ConfigurationHolder(IConfigurationUpdater configurationStore)
         {
             _configurationStore = configurationStore;
         }
 
+        public ConfigurationHolder() : this(new NullConfigurationUpdater()) { }
+
         public IObservable<Delta<Cluster>> ObserveClusters()
         {
-            throw new NotImplementedException();
+            return _clusterDeltas;
         }
 
         public async Task<ValueOrError<Cluster>> AddCluster(string name)
         {
             var s = await _configurationStore.AddCluster(name);
 
-            if (s.HasValue) _clusterOperations.OnNext(Delta.Create(s.Value, DeltaType.Added));
+            if (s.HasValue) _clusterDeltas.OnNext(Delta.Create(s.Value, DeltaType.Added));
 
             return s;
         }
@@ -35,7 +38,7 @@ namespace RElmah.Services
         {
             var s = await _configurationStore.RemoveCluster(name);
 
-            if (s.HasValue) _clusterOperations.OnNext(Delta.Create(Cluster.Create(name), DeltaType.Removed));
+            if (s.HasValue) _clusterDeltas.OnNext(Delta.Create(Cluster.Create(name), DeltaType.Removed));
 
             return s;
         }
---
+++ relmah/src/RElmah/Services/Nulls/NullConfigurationUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using RElmah.Models;
+using RElmah.Models.Configuration;
+
+namespace RElmah.Services.Nulls
+{
+    class NullConfigurationUpdater : IConfigurationUpdater
+    {
+        public Task<ValueOrError<Cluster>> AddCluster(string name)
+        {
+            return Task.FromResult((ValueOrError<Cluster>)null);
+        }
+
+        public Task<ValueOrError<bool>> RemoveCluster(string name)
+        {
+            return Task.FromResult((ValueOrError<bool>)null);
+        }
+    }
+}
