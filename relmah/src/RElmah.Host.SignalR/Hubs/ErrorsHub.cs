40c84d2f3c9bd30ce9a8b4216474b429d6ce3b6f
wasp <roby@robychechi.it>
2014-11-02 13:06:49 +0100

    Adding ErrorsHub with sample method

---
+++ relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace RElmah.Host.SignalR.Hubs
+{
+    [HubName("errors")]
+    public class ErrorsHub : Hub
+    {
+        public string Echo(string message)
+        {
+            return message;
+        }
+    }
+}
