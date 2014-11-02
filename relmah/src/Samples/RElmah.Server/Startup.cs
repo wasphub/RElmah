2ba3221707241512c8ad3fbd2ab4a70a531c9a5b
wasp <roby@robychechi.it>
2014-11-02 12:50:31 +0100

    Adding Startup class

---
+++ relmah/src/Samples/RElmah.Server/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RElmah.Server.Startup))]
+
+namespace RElmah.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", builder =>
+            {
+                builder.UseCors(CorsOptions.AllowAll);
+                builder.RunSignalR();
+            });
+
+            app.UseRElmah(cp => cp
+                .AddCluster("c1")
+                //.AddUserToCluster("wasp", "c1")
+                .AddApplication("foo", "foo", "c1"));
+        }
+    }
+}
