27d2efb302f5f719546b873b39fb8d332fcff439
wasp <roby@robychechi.it>
2014-11-02 13:31:50 +0100

    Typo

--- relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
+++ relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNet.SignalR.Hubs;
 
 namespace RElmah.Host.SignalR.Hubs
 {
-    [HubName("trelmah-errors")]
+    [HubName("relmah-errors")]
     public class ErrorsHub : Hub
     {
         public string Echo(string message)
