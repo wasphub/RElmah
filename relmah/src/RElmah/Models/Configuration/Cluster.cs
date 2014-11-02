cafea3549f7cc8d93daf5a3140d7b278281b5bdf
wasp <roby@robychechi.it>
2014-11-02 16:14:03 +0100

    Adding immutable collections

--- relmah/src/RElmah/Models/Configuration/Cluster.cs
+++ relmah/src/RElmah/Models/Configuration/Cluster.cs
@@ -1,7 +1,13 @@
-﻿namespace RElmah.Models.Configuration
+﻿using System.Collections.Generic;
+
+namespace RElmah.Models.Configuration
 {
     public class Cluster
     {
+        private readonly IDictionary<string, Application> _applications = new Dictionary<string, Application>();
+        private readonly IDictionary<string, User> _users = new Dictionary<string, User>();
+
+
         public string Name { get; private set; }
     }
 }
