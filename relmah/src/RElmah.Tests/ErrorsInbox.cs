0115743fa0a707f884b650d78f74df6b7a5bff72
wasp <roby@robychechi.it>
2014-11-02 11:56:43 +0100

    Preparing ErrorsInbox tests

---
+++ relmah/src/RElmah.Tests/ErrorsInbox.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace RElmah.Tests
+{
+    public class ErrorsInbox
+    {
+        [Fact]
+        public void IsTestingEnvisonmentOk()
+        {
+            Assert.True(true);
+        }
+    }
+}
