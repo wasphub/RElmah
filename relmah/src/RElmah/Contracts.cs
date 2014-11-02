359a44583eae85b29948ee3b382a5031f3f2a14e
wasp <roby@robychechi.it>
2014-11-02 11:44:46 +0100

    IErrorsInbox contract

---
+++ relmah/src/RElmah/Contracts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RElmah
+{
+    /// <summary>
+    /// This contract receives errors from the 
+    /// outside world, 'parks' them and makes
+    /// them observable. The 'parking' strategy
+    /// is left to the implementor.
+    /// </summary>
+    public interface IErrorsInbox
+    {
+        Task Post(ErrorPayload payload);
+        IObservable<ErrorPayload> GetErrorsStream();
+    }
+}
