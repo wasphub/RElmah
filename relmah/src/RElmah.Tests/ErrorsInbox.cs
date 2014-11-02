11ed34918260bbcebe9d225e37a20da27d298a49
wasp <roby@robychechi.it>
2014-11-02 12:14:58 +0100

    Making test succeed

--- relmah/src/RElmah.Tests/ErrorsInbox.cs
+++ relmah/src/RElmah.Tests/ErrorsInbox.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using RElmah.Models.Errors;
 using Xunit;
 
@@ -19,11 +20,17 @@ namespace RElmah.Tests
             ErrorPayload pushed = null;
             var inbox = new Services.ErrorsInbox();
 
+            inbox.GetErrorsStream().Subscribe(p =>
+            {
+                pushed = p;
+            });
+
             //Act
-            await inbox.Post(new ErrorPayload("foo", new Error(), "abc", string.Empty));
+            await inbox.Post(new ErrorPayload("sourceId", new Error(), "errorId", string.Empty));
 
             //Assert
             Assert.NotNull(pushed);
+            Assert.Equal("errorId", pushed.ErrorId);
         }
     }
 }
