86c04d5c2aa0777b0e900f912464857f0dfea93a
wasp <roby@robychechi.it>
2014-11-02 11:59:14 +0100

    Preparing failing test for Post

--- relmah/src/RElmah.Tests/ErrorsInbox.cs
+++ relmah/src/RElmah.Tests/ErrorsInbox.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System.Threading.Tasks;
+using RElmah.Models.Errors;
+using Xunit;
 
 namespace RElmah.Tests
 {
@@ -8,6 +10,20 @@ namespace RElmah.Tests
         public void IsTestingEnvisonmentOk()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public async Task Post()
+        {
+            //Arrange
+            ErrorPayload pushed = null;
+            var inbox = new Services.ErrorsInbox();
+
+            //Act
+            await inbox.Post(new ErrorPayload("foo", new Error(), "abc", string.Empty));
+
+            //Assert
+            Assert.NotNull(pushed);
         }
     }
 }
