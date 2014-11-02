e20cf1d7d94d85ab250b2fc53ea8d78a90c5624c
wasp <roby@robychechi.it>
2014-11-02 13:53:56 +0100

    Dictionary extensions

---
+++ relmah/src/RElmah/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RElmah.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TR Get<TK, TR>(this IDictionary<TK, TR> source, TK key, TR defaultValue)
+        {
+            return source.ContainsKey(key) ? source[key] : defaultValue;
+        }
+    }
+}
