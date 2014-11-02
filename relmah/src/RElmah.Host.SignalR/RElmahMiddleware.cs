7ee87306ee3e38f565514554b69e6067f0dd93d3
wasp <roby@robychechi.it>
2014-11-02 13:51:35 +0100

    Finalizing wiring the post-error handler with error decoding

--- relmah/src/RElmah.Host.SignalR/RElmahMiddleware.cs
+++ relmah/src/RElmah.Host.SignalR/RElmahMiddleware.cs
@@ -1,14 +1,32 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Owin;
+using RElmah.Middleware;
 
 namespace RElmah.Host.SignalR
 {
     public class RElmahMiddleware : OwinMiddleware
     {
+        private readonly Lazy<IErrorsInbox> _inbox;
+
+        private readonly IDictionary<string, Func<IDictionary<string, object>, Task>> _dispatchers;
+
         public RElmahMiddleware(OwinMiddleware next, IDependencyResolver resolver)
             : base(next)
         {
+            const string relmah = "relmah";
+
+            _inbox = new Lazy<IErrorsInbox>(resolver.Resolve<IErrorsInbox>);
+
+            var keyer = new Func<string, string>(s => string.Format("/{0}/{1}", relmah, s));
+
+            _dispatchers = new Dictionary<string, Func<IDictionary<string, object>, Task>>
+            {
+                { keyer("post-error"),   e => Routes.PostError(_inbox.Value, Routes.Elmah, e) }
+            };
+
         }
 
         public override Task Invoke(IOwinContext context)
--- relmah/src/RElmah/Middleware/Routes.cs
+++ relmah/src/RElmah/Middleware/Routes.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin;
+using Newtonsoft.Json;
 using RElmah.Models.Errors;
 
 namespace RElmah.Middleware
@@ -13,6 +16,23 @@ namespace RElmah.Middleware
             IDictionary<string, object> environment)
         {
             await inbox.Post(await executor( environment));
+        }
+
+        public async static Task<ErrorPayload> Elmah(IDictionary<string, object> environment)
+        {
+            var @params = await new OwinContext(environment).Request.ReadFormAsync();
+
+            var errorText = Decode(@params["error"]);
+            var sourceId = @params["sourceId"];
+            var errorId = @params["errorId"];
+            var infoUrl = @params["infoUrl"];
+
+            return new ErrorPayload(sourceId, JsonConvert.DeserializeObject<Error>(errorText), errorId, infoUrl);
+        }
+
+        static string Decode(string str)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(str));
         }
     }
 }
