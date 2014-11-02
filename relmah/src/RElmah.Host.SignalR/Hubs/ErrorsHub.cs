eac27ae52ff37b9c465110e7d33a97036ed2fd22
wasp <roby@robychechi.it>
2014-11-02 14:19:44 +0100

    Removing test Echo method

--- relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
+++ relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
@@ -6,9 +6,5 @@ namespace RElmah.Host.SignalR.Hubs
     [HubName("relmah-errors")]
     public class ErrorsHub : Hub
     {
-        public string Echo(string message)
-        {
-            return message;
-        }
     }
 }
