7f6aed62d9bcc8f112142b9e704c815fa457ed34
wasp <roby@robychechi.it>
2014-11-02 13:47:13 +0100

    Basic routing method for porting errors

--- relmah/src/RElmah/Middleware/Routes.cs
+++ relmah/src/RElmah/Middleware/Routes.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using RElmah.Models.Errors;
 
 namespace RElmah.Middleware
 {
     public static class Routes
     {
+        public async static Task PostError(
+            IErrorsInbox inbox,
+            Func<IDictionary<string, object>, Task<ErrorPayload>> executor,
+            IDictionary<string, object> environment)
+        {
+            await inbox.Post(await executor( environment));
+        }
     }
 }
