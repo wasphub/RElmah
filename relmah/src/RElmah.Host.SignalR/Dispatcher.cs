a36a22174daa5415b66d81bc51b15a5e424fdb09
wasp <roby@robychechi.it>
2014-11-02 14:06:53 +0100

    Preparing a dispatcher's implementation

---
+++ relmah/src/RElmah.Host.SignalR/Dispatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RElmah.Models.Errors;
+
+namespace RElmah.Host.SignalR
+{
+    public class Dispatcher : IDispatcher
+    {
+        public Dispatcher(IErrorsInbox errorsInbox)
+        {
+            errorsInbox.GetErrorsStream().Subscribe(p => DispatchError(p));
+        }
+
+        public Task DispatchError(ErrorPayload payload)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
