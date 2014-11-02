7ee1bfdede6d2075d2dd7f56cdd23f2da254865a
wasp <roby@robychechi.it>
2014-11-02 14:09:02 +0100

    Preparing basic errors notification

--- relmah/src/RElmah.Host.SignalR/Dispatcher.cs
+++ relmah/src/RElmah.Host.SignalR/Dispatcher.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using RElmah.Host.SignalR.Hubs;
 using RElmah.Models.Errors;
 
 namespace RElmah.Host.SignalR
 {
     public class Dispatcher : IDispatcher
     {
+        private readonly IHubContext _context = GlobalHost.ConnectionManager.GetHubContext<ErrorsHub>();
+
         public Dispatcher(IErrorsInbox errorsInbox)
         {
             errorsInbox.GetErrorsStream().Subscribe(p => DispatchError(p));
@@ -13,7 +17,7 @@ namespace RElmah.Host.SignalR
 
         public Task DispatchError(ErrorPayload payload)
         {
-            throw new NotImplementedException();
+            return _context.Clients.All.error(payload);
         }
     }
 }
