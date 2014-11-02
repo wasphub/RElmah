f819bccde41fd731ff90cac25789ab1d7a742a47
wasp <roby@robychechi.it>
2014-11-02 11:50:13 +0100

    Implementing IErrorsInbox service

---
+++ relmah/src/RElmah/Services/ErrorsInbox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using RElmah.Models.Errors;
+
+namespace RElmah.Services
+{
+    public class ErrorsInbox : IErrorsInbox
+    {
+        public Task Post(ErrorPayload payload)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<ErrorPayload> GetErrorsStream()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
