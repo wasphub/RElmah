2230dada9c9e945c463ac0989b88a4a3259ac6e0
wasp <roby@robychechi.it>
2014-11-02 16:47:32 +0100

    Type to describe variation over a target

---
+++ relmah/src/RElmah/Models/Delta.cs
@@ -0,0 +1,29 @@
+﻿namespace RElmah.Models
+{
+    public enum DeltaType
+    {
+        Added,
+        Removed,
+        Updated
+    }
+
+    public static class Delta
+    {
+        public static Delta<T> Create<T>(T target, DeltaType type) where T : class
+        {
+            return new Delta<T>(target, type);
+        }
+    }
+
+    public class Delta<T> where T : class
+    {
+        public Delta(T target, DeltaType type)
+        {
+            Target = target;
+            Type = type;
+        }
+
+        public T Target { get; private set; }
+        public DeltaType Type { get; private set; }
+    }
+}
