ca47718b559832b29ae7fe6a6f48ddc7f78ffb38
wasp <roby@robychechi.it>
2014-11-03 23:17:14 +0100

    Relationship type to express changes on related objects

---
+++ relmah/src/RElmah/Models/Relationship.cs
@@ -0,0 +1,22 @@
+﻿namespace RElmah.Models
+{
+    public static class Relationship
+    {
+        public static Relationship<TP, TS> Create<TP, TS>(TP primary, TS secondary)
+        {
+            return new Relationship<TP, TS>(primary, secondary);
+        }
+    }
+
+    public class Relationship<TP, TS>
+    {
+        public Relationship(TP primary, TS secondary)
+        {
+            Primary = primary;
+            Secondary = secondary;
+        }
+
+        public TP Primary { get; private set; }
+        public TS Secondary { get; private set; }
+    }
+}
