bcbaed972c9c15c077a8b451728dfb0dee2c62c7
wasp <roby@robychechi.it>
2014-11-03 22:52:50 +0100

    Introducing ConcurrentReference for safe concurrent handles on reference types (Interlocked.CompareExchange is used to swap old and new value safely)

---
+++ relmah/src/RElmah/Models/ConcurrentReference.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace RElmah.Models
+{
+    public class ConcurrentReference<T> where T : class
+    {
+        private volatile T _value;
+
+        public ConcurrentReference(T value)
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get { return _value; }
+        }
+
+        public T Mutate(Func<T, T> mutator)
+        {
+            var original = _value;
+            while (true)
+            {
+                var mutated = mutator(_value);
+
+                // ReSharper disable CSharpWarnings::CS0420
+                var current = Interlocked.CompareExchange(ref _value, mutated, original);
+                // ReSharper restore CSharpWarnings::CS0420
+
+                if (current == original)
+                    return original;
+
+                original = current;
+            }
+        }
+    }
+}
