7c6c87ae36b5d9972e9c03bd40af1b8b1783b2e8
wasp <roby@robychechi.it>
2014-11-03 23:33:53 +0100

    Handling changes on cluster's users

--- relmah/src/RElmah.Host.SignalR/Dispatcher.cs
+++ relmah/src/RElmah.Host.SignalR/Dispatcher.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using RElmah.Host.SignalR.Hubs;
+using RElmah.Models;
+using RElmah.Models.Configuration;
 using RElmah.Models.Errors;
 
 namespace RElmah.Host.SignalR
@@ -10,9 +12,19 @@ namespace RElmah.Host.SignalR
     {
         private readonly IHubContext _context = GlobalHost.ConnectionManager.GetHubContext<ErrorsHub>();
 
-        public Dispatcher(IErrorsInbox errorsInbox)
+        public Dispatcher(IErrorsInbox errorsInbox, IConfigurationProvider configurationProvider)
         {
             errorsInbox.GetErrorsStream().Subscribe(p => DispatchError(p));
+
+            configurationProvider.ObserveClusterUsers().Subscribe(p => DispatchUserApplications(p));
+        }
+
+        public Task DispatchUserApplications(Delta<Relationship<Cluster, User>> payload)
+        {
+            return _context
+                .Clients
+                .User(payload.Target.Secondary.Name)
+                .applications(payload.Target.Primary.Applications);
         }
 
         public Task DispatchError(ErrorPayload payload)
