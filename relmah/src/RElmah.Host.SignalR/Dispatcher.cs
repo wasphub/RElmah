e2884f4d034eb82df475993ce9d078f4d71cc950
wasp <roby@robychechi.it>
2014-11-05 00:14:25 +0100

    Enhancing dispatcher's capabilities (still draft...)

--- relmah/src/RElmah.Host.SignalR/Dispatcher.cs
+++ relmah/src/RElmah.Host.SignalR/Dispatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using RElmah.Host.SignalR.Hubs;
@@ -10,29 +11,45 @@ namespace RElmah.Host.SignalR
 {
     public class Dispatcher : IDispatcher
     {
-        private readonly IConfigurationProvider _configurationProvider;
+        private readonly IConfigurationUpdater  _configurationUpdater;
         private readonly IHubContext _context = GlobalHost.ConnectionManager.GetHubContext<ErrorsHub>();
 
-        public Dispatcher(IErrorsInbox errorsInbox, IConfigurationProvider configurationProvider)
+        public Dispatcher(IErrorsInbox errorsInbox, IConfigurationProvider configurationProvider, IConfigurationUpdater configurationUpdater)
         {
-            _configurationProvider = configurationProvider;
+            _configurationUpdater  = configurationUpdater;
 
             errorsInbox.GetErrorsStream().Subscribe(p => DispatchError(p));
 
-            configurationProvider.ObserveClusterUsers().Subscribe(p => DispatchUserApplications(p));
+            configurationProvider.ObserveClusterUsers().Subscribe(DispatchUserApplications);
+            configurationProvider.ObserveClusterApplications().Subscribe(DispatchUsersApplication);
         }
 
-        public Task DispatchUserApplications(Delta<Relationship<Cluster, User>> payload)
+        public void DispatchUserApplications(Delta<Relationship<Cluster, User>> payload)
         {
-            return _context
+            _context
                 .Clients
                 .User(payload.Target.Secondary.Name)
-                .applications(payload.Target.Primary.Applications);
+                .applications(from a in payload.Target.Primary.Applications select a.Name);
+        }
+
+        public void DispatchUsersApplication(Delta<Relationship<Cluster, Application>> payload)
+        {
+            foreach (var user in payload.Target.Primary.Users)
+                _context
+                    .Clients
+                    .User(user.Name)
+                    .applications(from a in payload.Target.Primary.Applications select a.Name);
         }
 
         public Task DispatchError(ErrorPayload payload)
         {
             return _context.Clients.Group(payload.SourceId).error(payload);
+        }
+
+        public void Connect(string user, Action<string> connector)
+        {
+            foreach (var app in _configurationUpdater.GetUserApplications(user))
+                connector(app.Name);
         }
     }
 }
