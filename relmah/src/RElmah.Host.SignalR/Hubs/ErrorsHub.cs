39de395d40e62fd670e3c2fd4f63bf670ea34e6a
wasp <roby@robychechi.it>
2014-11-05 00:13:09 +0100

    Handling dep injection + OnConnected

--- relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
+++ relmah/src/RElmah.Host.SignalR/Hubs/ErrorsHub.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 
@@ -7,6 +8,22 @@ namespace RElmah.Host.SignalR.Hubs
     [HubName("relmah-errors")]
     public class ErrorsHub : Hub
     {
+        private readonly IDispatcher _dispatcher;
+        private readonly IUserIdProvider _userIdProvider;
+
+        public ErrorsHub(IDispatcher dispatcher, IUserIdProvider userIdProvider)
+        {
+            _dispatcher = dispatcher;
+            _userIdProvider = userIdProvider;
+        }
+
+        public override Task OnConnected()
+        {
+            _dispatcher.Connect( _userIdProvider.GetUserId(Context.Request), a => Groups.Add(Context.ConnectionId, a));
+
+            return base.OnConnected();
+        }
+
         public void Monitor(IEnumerable<string> apps)
         {
             foreach (var app in apps)
