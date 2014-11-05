766b8db73f52ef0fff6310ef6c4236f1550341c9
wasp <roby@robychechi.it>
2014-11-05 20:43:38 +0100

    Enumerable extensions

---
+++ relmah/src/RElmah/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RElmah.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Each<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null) yield break;
+
+            foreach (var item in source)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+    }
+}
