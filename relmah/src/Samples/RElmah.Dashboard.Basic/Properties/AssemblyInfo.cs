249710e1fa7176e93f7ebd1dd5a7e27dfd606b9a
wasp <roby@robychechi.it>
2014-11-09 16:28:33 +0100

    Renaming sample client project

--- relmah/src/Samples/RElmah.Dashboard.Basic/Properties/AssemblyInfo.cs
+++ relmah/src/Samples/RElmah.Dashboard.Basic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RElmah.Client")]
+[assembly: AssemblyTitle("RElmah.Dashboard.Basic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RElmah.Client")]
+[assembly: AssemblyProduct("RElmah.Dashboard.Basic")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
