ee71707c9c6cc431fc377ee303123968eb96e30c
wasp <roby@robychechi.it>
2014-11-09 16:42:29 +0100

    Basic Connection type

---
+++ relmah/src/RElmah.Client/Connection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using RElmah.Models.Errors;
+
+namespace RElmah.Client
+{
+    public class Connection : IDisposable
+    {
+        private readonly HubConnection _connection;
+
+        private readonly Subject<ErrorPayload> _errors = new Subject<ErrorPayload>();
+
+        public Connection(string endpoint)
+        {
+            _connection = new HubConnection(endpoint);
+
+            var proxy = _connection.CreateHubProxy("relmah-errors");
+
+            proxy.On<ErrorPayload>(
+                "error",
+                p => _errors.OnNext(p));
+        }
+
+        public Task Start()
+        {
+            return _connection.Start();
+        }
+
+        public IObservable<ErrorPayload> Errors { get { return _errors; } }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+
+            _errors.OnCompleted();
+            _errors.Dispose();
+        }
+    }
+}
