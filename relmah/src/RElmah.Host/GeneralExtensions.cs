0081e614445c3d935e400fa349e19250cf10f4aa
wasp <roby@robychechi.it>
2014-11-22 14:40:09 +0100

    target cannot be null (not strictly necessary, but it looks better)

--- relmah/src/RElmah.Host/GeneralExtensions.cs
+++ relmah/src/RElmah.Host/GeneralExtensions.cs
@@ -6,14 +6,14 @@ namespace RElmah.Host
 {
     public static class GeneralExtensions
     {
-        public static TR SafeCall<TT, TR>(this TT target, Func<TT, TR> call, Func<TR> @default, params Func<bool>[] guards)
+        public static TR SafeCall<TT, TR>(this TT target, Func<TT, TR> call, Func<TR> @default, params Func<bool>[] guards) where TT : class
         {
             var checks =
                 from g in guards
                 let success = g.Catch()
                 select success.HasValue && success.Value;
 
-            return checks.TakeWhile(c => c).Count() == guards.Length 
+            return target != null && checks.TakeWhile(c => c).Count() == guards.Length 
                  ? call(target) 
                  : @default();
         }
