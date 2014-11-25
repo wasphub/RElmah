365c68168a52d4f5512d2dd04c3923878a40e390
wasp <roby@robychechi.it>
2014-11-25 22:15:20 +0100

    Regex patterns

--- relmah/src/RElmah/Middleware/RElmahMiddleware.cs
+++ relmah/src/RElmah/Middleware/RElmahMiddleware.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using RElmah.Extensions;
@@ -9,40 +11,72 @@ namespace RElmah.Middleware
 {
     public class RElmahMiddleware : OwinMiddleware
     {
-        private readonly IDictionary<string, Func<IDictionary<string, object>, Task>> _dispatchers;
+        private readonly IDictionary<string, Func<IDictionary<string, object>, IDictionary<string, string>, Task>> _routes;
 
         public RElmahMiddleware(OwinMiddleware next, IResolver resolver)
             : base(next)
         {
             const string relmah = "relmah";
 
-            var inbox   = new Lazy<IErrorsInbox>(resolver.Resolve<IErrorsInbox>);
+            var inbox = new Lazy<IErrorsInbox>(resolver.Resolve<IErrorsInbox>);
             var updater = new Lazy<IConfigurationUpdater>(resolver.Resolve<IConfigurationUpdater>);
 
-            var keyer = new Func<string, string>(s => string.Format("/{0}/{1}", relmah, s));
+            var keyer = new Func<string, string>(s => string.Format("/{0}/{1}", relmah, ToRegex(s)));
 
-            _dispatchers = new Dictionary<string, Func<IDictionary<string, object>, Task>>
+            _routes = new Dictionary<string, Func<IDictionary<string, object>, IDictionary<string, string>, Task>>
             {
-                { keyer("post-error"),           e => Routes.PostError(inbox.Value, e) },
-                                                 
-                { keyer("clusters"),             e => Routes.Clusters(updater.Value, e) },
-                { keyer("applications"),         e => Routes.Applications(updater.Value, e) },
-                { keyer("users"),                e => Routes.Users(updater.Value, e) },
-                                                 
-                { keyer("cluster-users"),        e => Routes.ClusterUsers(updater.Value, e) },
-                { keyer("cluster-applications"), e => Routes.ClusterApplications(updater.Value, e) },
+                { keyer("post-error"),                       (e, ks) => Routes.PostError(inbox.Value, e, ks) },
+                
+                { keyer("clusters/{cluster}/apps/{app}"),    (e, ks) => Routes.ClusterApplications(updater.Value, e, ks) },
+                { keyer("clusters/{cluster}/apps"),          (e, ks) => Routes.ClusterApplications(updater.Value, e, ks) },
+                { keyer("clusters/{cluster}/users/{user}"),  (e, ks) => Routes.ClusterUsers(updater.Value, e, ks) },
+                { keyer("clusters/{cluster}/users"),         (e, ks) => Routes.ClusterUsers(updater.Value, e, ks) },
+                { keyer("clusters/{cluster}"),               (e, ks) => Routes.Clusters(updater.Value, e, ks) },
+                { keyer("clusters"),                         (e, ks) => Routes.Clusters(updater.Value, e, ks) },
+                
+                { keyer("applications/(?'app'.*)"),          (e, ks) => Routes.Applications(updater.Value, e, ks) },
+                { keyer("applications"),                     (e, ks) => Routes.Applications(updater.Value, e, ks) },
+                
+                { keyer("users/(?'user'.*)"),                (e, ks) => Routes.Users(updater.Value, e, ks) },                
+                { keyer("users"),                            (e, ks) => Routes.Users(updater.Value, e, ks) },
             };
-
         }
-         
+
+        static string ToRegex(string pattern)
+        {
+            return Regex.Replace(pattern, @"\{(?'x'\w*)\}", @"(?'$1'.*)");
+        }
+
         public override Task Invoke(IOwinContext context)
         {
-            var request  = new OwinRequest(context.Environment);
+            var request = new OwinRequest(context.Environment);
             var segments = request.Uri.Segments;
-            var path     = string.Join(null, segments.Take(2).Concat(segments.Skip(2).Take(1).Select(s => s.Replace("/", ""))));
-            var key      = new PathString(path).ToString();
+            var raw = string.Join(null, segments);
 
-            return _dispatchers.Get(key, e => Next.Invoke(context))(context.Environment);
+            var matches =
+                from kvp in _routes
+                let matcher = new Regex(kvp.Key)
+                let match = matcher.Match(raw)
+                where match.Success
+                let route = _routes[kvp.Key]
+                let groups = match.Groups
+                    .Cast<Group>()
+                    .Index()
+                select new
+                {
+                    Params =
+                        from g in groups
+                        select new KeyValuePair<string, string>(
+                            matcher.GroupNameFromNumber(g.Key),
+                            g.Value.Value),
+                    Route = route
+                };
+
+            var invocation = matches.FirstOrDefault();
+
+            return invocation != null 
+                 ? invocation.Route(context.Environment, invocation.Params.Where(k => k.Key != "0").ToDictionary(k => k.Key, v => v.Value)) 
+                 : Next.Invoke(context);
         }
     }
 }
