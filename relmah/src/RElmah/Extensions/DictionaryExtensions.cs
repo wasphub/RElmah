9651f6caf35546bec2fee32fb44f9b59dc3944eb
wasp <roby@robychechi.it>
2014-11-30 17:58:16 +0100

    New extensions

--- relmah/src/RElmah/Extensions/DictionaryExtensions.cs
+++ relmah/src/RElmah/Extensions/DictionaryExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RElmah.Extensions
 {
@@ -12,6 +14,31 @@ namespace RElmah.Extensions
         public static TR Get<TK, TR>(this IReadOnlyDictionary<TK, TR> source, TK key, TR defaultValue)
         {
             return source.ContainsKey(key) ? source[key] : defaultValue;
+        }
+
+        public static TR Get<TK, TR>(this IDictionary<TK, TR> source, TK key) where TR : class
+        {
+            return source.ContainsKey(key) ? source[key] : null;
+        }
+
+        public static TR Get<TK, TR>(this IReadOnlyDictionary<TK, TR> source, TK key) where TR : class
+        {
+            return source.ContainsKey(key) ? source[key] : null;
+        }
+
+        public static KeyValuePair<TK, TV> AsKeyFor<TK, TV>(this TK key, TV value)
+        {
+            return new KeyValuePair<TK, TV>(key, value);
+        }
+
+        public static IDictionary<TK, TR> ToDictionary<TK, TR>(this IEnumerable<KeyValuePair<TK, TR>> source)
+        {
+            return source.ToDictionary(k => k.Key, v => v.Value);
+        }
+
+        public static IDictionary<TK, TR> Select<TK, TV, TR>(this IEnumerable<KeyValuePair<TK, TV>> source, Func<TV, TR> selector)
+        {
+            return source.ToDictionary(k => k.Key, v => selector(v.Value));
         }
     }
 }
