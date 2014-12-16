06da4082745b5c34ef223b98a43106d8c7f536eb
wasp <roby@robychechi.it>
2014-12-16 13:28:32 +0100

    Add untracked after latest adjustments

---
+++ relmah/src/RElmah/Middleware/ErrorsMiddleware.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using RElmah.Common;
+using RElmah.Extensions;
+
+namespace RElmah.Middleware
+{
+    public class ErrorsMiddleware : OwinMiddleware
+    {
+        public override Task Invoke(IOwinContext context)
+        {
+            return Router.Invoke(context, Next.Invoke);
+        }
+
+        public ErrorsMiddleware(OwinMiddleware next, IResolver resolver)
+            : base(next)
+        {
+            var inbox = new Lazy<IErrorsInbox>(resolver.Resolve<IErrorsInbox>);
+
+            Router.Build(builder => builder
+
+                .ForRoute("post-error", route => route
+                    .Post(async (environment, keys, form) =>
+                    {
+                        var errorText = Encoding.UTF8.GetString(Convert.FromBase64String(form.Get("error")));
+                        var sourceId = form.Get("sourceId");
+                        var errorId = form.Get("errorId");
+                        var infoUrl = form.Get("infoUrl");
+
+                        var payload = new ErrorPayload(sourceId, JsonConvert.DeserializeObject<Error>(errorText), errorId, infoUrl);
+
+                        await inbox.Value.Post(payload);
+
+                        return payload;
+                    })
+                )
+            );
+        }
+    }
+}
---
+++ relmah/src/Samples/RElmah.Dashboard.Basic/Scripts/relmah/index.js
@@ -0,0 +1,19 @@
+﻿(function($) {
+    "use strict";
+
+    function View(opts) {
+        this.$ = $(opts.el);
+        this.template = _.template($(opts.template).text());
+    }
+
+    View.prototype.render = function (data) {
+        this.$.html(this.template({ data: data }));
+    };
+
+    function ErrorsView(opts) {
+        View.apply(this, arguments);
+
+        opts.errors.subscribe(this.render.bind(this));
+    };
+
+})($);
