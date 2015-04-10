fdd3907754c07e1fc82e156e6bf7c493913a1568
wasp <roby@robychechi.it>
2015-04-10 17:43:02 +0200

    More rational test case names

--- relmah/src/Samples/RElmah.Server/Startups.cs
+++ relmah/src/Samples/RElmah.Server/Startups.cs
@@ -42,21 +42,21 @@ namespace RElmah.Server
                     {
                         DomainConfigurator = async conf =>
                         {
-                            var cFoo = await conf.AddCluster("foo");
+                            var c01 = await conf.AddCluster("c01");
 
-                            var e7001 = await conf.AddSource("e7001");
+                            var s01 = await conf.AddSource("s01");
 
                             //For Windows Auth testing
-                            var cu = await conf.AddUser(string.Format(@"{0}\{1}", Environment.UserDomainName, Environment.UserName));
+                            var wcu = await conf.AddUser(string.Format(@"{0}\{1}", Environment.UserDomainName, Environment.UserName));
 
                             //For client token testing
-                            var wasp = await conf.AddUser("wasp");
+                            var u01 = await conf.AddUser("u01");
 
                             Task.WaitAll(
-                                conf.AddSourceToCluster(cFoo.Value.Name, e7001.Value.SourceId),
+                                conf.AddSourceToCluster(c01.Value.Name, s01.Value.SourceId),
 
-                                conf.AddUserToCluster(cFoo.Value.Name, cu.Value.Name),
-                                conf.AddUserToCluster(cFoo.Value.Name, wasp.Value.Name)
+                                conf.AddUserToCluster(c01.Value.Name, wcu.Value.Name),
+                                conf.AddUserToCluster(c01.Value.Name, u01.Value.Name)
                             );
                         }
                     })
@@ -98,32 +98,32 @@ namespace RElmah.Server
                     {
                         DomainConfigurator = async conf =>
                         {
-                            var cFoo = await conf.AddCluster("foo");
-                            var cBar = await conf.AddCluster("bar");
-
-                            var e7001 = await conf.AddSource("e7001");
-                            var e7002 = await conf.AddSource("e7002");
-                            var e7003 = await conf.AddSource("e7003");
+                            var c01 = await conf.AddCluster("c01");
+                            var c02 = await conf.AddCluster("c02");
+                                    
+                            var s01 = await conf.AddSource("s01");
+                            var s02 = await conf.AddSource("s02");
+                            var s03 = await conf.AddSource("s03");
 
                             //For Windows Auth testing
-                            var cu = await conf.AddUser(string.Format(@"{0}\{1}", Environment.UserDomainName, Environment.UserName));
+                            var wcu = await conf.AddUser(string.Format(@"{0}\{1}", Environment.UserDomainName, Environment.UserName));
 
                             //For client token testing
-                            var wasp = await conf.AddUser("wasp");
-                            var cuki = await conf.AddUser("cuki");
+                            var u01 = await conf.AddUser("u01");
+                            var u02 = await conf.AddUser("u02");
                             var all = await conf.AddUser("all");
 
                             Task.WaitAll(
-                                conf.AddSourceToCluster(cFoo.Value.Name, e7001.Value.SourceId),
-                                conf.AddSourceToCluster(cBar.Value.Name, e7002.Value.SourceId),
-                                conf.AddSourceToCluster(cBar.Value.Name, e7003.Value.SourceId),
+                                conf.AddSourceToCluster(c01.Value.Name, s01.Value.SourceId),
+                                conf.AddSourceToCluster(c02.Value.Name, s02.Value.SourceId),
+                                conf.AddSourceToCluster(c02.Value.Name, s03.Value.SourceId),
 
-                                conf.AddUserToCluster(cFoo.Value.Name, cu.Value.Name),
-                                conf.AddUserToCluster(cFoo.Value.Name, wasp.Value.Name),
-                                conf.AddUserToCluster(cFoo.Value.Name, all.Value.Name),
+                                conf.AddUserToCluster(c01.Value.Name, wcu.Value.Name),
+                                conf.AddUserToCluster(c01.Value.Name, u01.Value.Name),
+                                conf.AddUserToCluster(c01.Value.Name, all.Value.Name),
 
-                                conf.AddUserToCluster(cBar.Value.Name, cuki.Value.Name),
-                                conf.AddUserToCluster(cBar.Value.Name, all.Value.Name)
+                                conf.AddUserToCluster(c02.Value.Name, u02.Value.Name),
+                                conf.AddUserToCluster(c02.Value.Name, all.Value.Name)
                             );
                         }
                     })
