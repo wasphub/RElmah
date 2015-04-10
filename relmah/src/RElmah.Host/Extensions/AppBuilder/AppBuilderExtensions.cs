b4442c526eeba8cbdebbee58614335de42450d17
wasp <roby@robychechi.it>
2015-04-10 17:14:23 +0200

    Removing obsolete methods

--- relmah/src/RElmah.Host/Extensions/AppBuilder/AppBuilderExtensions.cs
+++ relmah/src/RElmah.Host/Extensions/AppBuilder/AppBuilderExtensions.cs
@@ -8,58 +8,11 @@ using RElmah.Middleware;
 using RElmah.Middleware.Bootstrapping;
 using RElmah.Middleware.Bootstrapping.Builder;
 using RElmah.Models.Settings;
-using RElmah.Notifiers;
 
 namespace RElmah.Host.Extensions.AppBuilder
 {
     public static class AppBuilderExtensions
     {
-        public static IAppBuilder UseRElmahObsolete(this IAppBuilder builder, Settings settings = null)
-        {
-            var registry = new Registry();
-
-            var frontend = new FrontendNotifier();
-
-            var ip       = settings != null && settings.Bootstrap != null && settings.Bootstrap.IdentityProviderBuilder != null
-                         ? settings.Bootstrap.IdentityProviderBuilder()
-                         : new WindowsPrincipalIdentityProvider();
-
-            var bp       = registry.PrepareObsolete(frontend, 
-                (ep, ei, dh) => new FrontendBackendNotifier(ep, ei, dh),
-                () => new BackendFrontendNotifier(), 
-                ip, 
-                (fqf, bqf, ei, dh, bn) => new { fqf, bqf, ei, dh, bn }, 
-                settings);
-
-            var dp       = new DelegatingUserIdProvider(ip);
-
-            registry.Register(typeof(IUserIdProvider), () => dp);
-
-            //Frontend or Backend?
-            if (settings != null && settings.Bootstrap != null && settings.Bootstrap.RunBackend)
-                registry.Register(typeof(BackendHub), () => new BackendHub());
-            else
-                registry.Register(typeof(ErrorsHub), () => new ErrorsHub(bp.fqf, dp));
-
-            if (settings != null && settings.Bootstrap != null && settings.Bootstrap.Registry != null)
-                settings.Bootstrap.Registry(registry);
-
-            if (settings != null && settings.Bootstrap != null && settings.Bootstrap.Domain != null)
-                settings.Bootstrap.Domain(bp.dh);
-
-            if (settings != null && settings.Errors != null)
-                builder = settings.Errors.UseRandomizer
-                        ? builder.UseRElmahMiddleware<RandomSourceErrorsMiddleware>(bp.ei, bp.dh, settings.Errors)
-                        : builder.UseRElmahMiddleware<ErrorsMiddleware>(bp.ei, settings.Errors);
-
-            if (settings != null && settings.Domain != null)
-                builder = builder.UseRElmahMiddleware<DomainMiddleware>(bp.dh, settings.Domain);
-
-            bp.bqf.Setup();
-            
-            return builder;
-        }
-
         static IAppBuilder UseRElmahMiddleware<T>(this IAppBuilder builder, params object[] args)
         {
             return builder.Use(typeof(T), args);
--- relmah/src/RElmah/Middleware/Bootstrapping/Bootstrapper.cs
+++ relmah/src/RElmah/Middleware/Bootstrapping/Bootstrapper.cs
@@ -2,7 +2,6 @@ using System;
 using RElmah.Domain;
 using RElmah.Errors;
 using RElmah.Middleware.Bootstrapping.Builder;
-using RElmah.Models.Settings;
 using RElmah.Notifiers;
 using RElmah.Publishers;
 using RElmah.Queries;
@@ -17,57 +16,6 @@ namespace RElmah.Middleware.Bootstrapping
 {
     public static class Bootstrapper
     {
-        public static T PrepareObsolete<T>(
-            this IRegistry registry, 
-            IFrontendNotifier frontendNotifier,
-            Func<string, IErrorsInbox, IDomainPersistor, IBackendNotifier> frontendBackendNotifierCreator,
-            Func<IBackendNotifier> backendFrontendNotifierCreator, 
-            IIdentityProvider identityProvider,
-            Func<IFrontendQueriesFactory, IBackendQueriesFactory, IErrorsInbox, IDomainPersistor, IBackendNotifier, T> resultor,
-            Settings settings = null)
-        {
-            var bl = new InMemoryErrorsBacklog();
-            var ei = new QueuedErrorsInbox(bl);
-
-            var bi = new QueuedErrorsInbox();   //for backend only
-
-            var ds = settings != null && settings.Domain != null && settings.Domain.DomainStoreBuilder != null
-                   ? settings.Domain.DomainStoreBuilder()
-                   : new InMemoryDomainStore();
-
-            var dh = new DomainHolder(ds);
-
-            var fqf = new QueriesFactory(ei, bi, bl, dh, dh, frontendNotifier,
-                     () => new ErrorsQuery(),
-                     () => new RecapsQuery());
-
-            var bn  = NullBackendNotifier.Instance;
-
-            if (settings != null && settings.Bootstrap != null && !string.IsNullOrWhiteSpace(settings.Bootstrap.TargetBackendEndpoint))
-            {
-                bn  = frontendBackendNotifierCreator(settings.Bootstrap.TargetBackendEndpoint, bi, dh);
-            }
-            else if (settings != null && settings.Bootstrap != null && settings.Bootstrap.RunBackend)
-            {
-                bn = backendFrontendNotifierCreator();
-            }
-
-            var bqf = new Queries.Backend.QueriesFactory(ei, bl, dh, dh, bn,
-                () => new ErrorsBusQuery(),
-                () => new ConfigurationBusQuery(skipEventsFromBackend: settings == null || settings.Bootstrap == null || settings.Bootstrap.RunBackend));
-
-            //Infrastructure
-            registry.Register(typeof(IErrorsBacklog),          () => bl);
-            registry.Register(typeof(IErrorsInbox),            () => ei);
-            registry.Register(typeof(IDomainPublisher),        () => dh);
-            registry.Register(typeof(IDomainPersistor),        () => dh);
-            registry.Register(typeof(IDomainStore),            () => ds);
-            registry.Register(typeof(IFrontendQueriesFactory), () => fqf);
-            registry.Register(typeof(IBackendQueriesFactory),  () => bqf);
-
-            return resultor(fqf, bqf, ei, dh, bn);
-        }
-
         public static T Prepare<T>(
             this IRegistry registry,
             IFrontendNotifier frontendNotifier,
@@ -106,7 +54,7 @@ namespace RElmah.Middleware.Bootstrapping
 
             var bqf = new Queries.Backend.QueriesFactory(ei, bl, dh, dh, bn,
                 () => new ErrorsBusQuery(),
-                () => new ConfigurationBusQuery(skipEventsFromBackend: settings.Side == Side.Backend));
+                () => new ConfigurationBusQuery(settings.Side == Side.Backend));
 
             //Infrastructure
             registry.Register(typeof(IErrorsBacklog), () => bl);
